79e00cf92dd0df57b88c03558c591ce0a14b6199
shenqiangbin <shenqb@qiezzi.com>
2015-12-26 15:21:42 +0800

    主要为更新nuget包

--- demo02/App_Start/RouteConfig.cs
+++ demo02/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace demo02
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "LuceneNet", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ demo02/Controllers/LuceneNetController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo02.Controllers
+{
+    public class LuceneNetController : Controller
+    {
+        // GET: LuceneNet
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Demo1()
+        {
+            return View();
+        }
+
+        public ActionResult CreateIndex()
+        {
+            string indexPath = HttpContext.Server.MapPath("~/IndexData");
+
+            //FSDirectory
+
+            return Content(indexPath);
+
+        }
+    }
+}
