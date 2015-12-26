973aa5a12af8cd7cfb6b8ea7e1fa0874e6269e1b
shenqiangbin <shenqb@qiezzi.com>
2015-12-26 16:20:04 +0800

    添加创建索引库的方法

--- demo02/Controllers/LuceneNetController.cs
+++ demo02/Controllers/LuceneNetController.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using demo02.Models;
+using Lucene.Net.Analysis.PanGu;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,12 +27,58 @@ namespace demo02.Controllers
 
         public ActionResult CreateIndex()
         {
+            var directory = CreateFSDirectory();
+            bool isExist = IndexReader.IndexExists(directory);
+
+            IndexWriter writer = new IndexWriter(directory, new PanGuAnalyzer(), !isExist, IndexWriter.MaxFieldLength.UNLIMITED);
+
+            GetNews().ForEach(m =>
+            {
+                writer.AddDocument(NewsToDocument(m));
+            });
+
+            writer.Dispose();
+            directory.Dispose();
+
+            return Content("ok");
+        }
+
+        private FSDirectory CreateFSDirectory()
+        {
             string indexPath = HttpContext.Server.MapPath("~/IndexData");
 
-            //FSDirectory
+            FSDirectory directory = FSDirectory.Open(new DirectoryInfo(indexPath), new NativeFSLockFactory());
 
-            return Content(indexPath);
+            bool isExist = IndexReader.IndexExists(directory);
+            if (isExist)
+            {
+                if (IndexWriter.IsLocked(directory))
+                    IndexWriter.Unlock(directory);
+            }
 
+            return directory;
+        }
+
+        private List<News> GetNews()
+        {
+            List<News> list = new List<News>();
+
+            list.Add(new News { Id = Guid.NewGuid(), Title = "ASP.NET验证问题", Content = "验证什么的是灰常重要的.... ASP.NET" });
+            list.Add(new News { Id = Guid.NewGuid(), Title = "Web API验证问题", Content = "验证什么的是灰常重要的....Web API" });
+            list.Add(new News { Id = Guid.NewGuid(), Title = "ASP.NET登录", Content = "登录问题也是一个大问题" });
+
+            return list;
+        }
+
+        private Document NewsToDocument(News news)
+        {
+            Document document = new Document();
+
+            document.Add(new Field("id", news.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            document.Add(new Field("title", news.Title.ToString(), Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            document.Add(new Field("content", news.Content.ToString(), Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+
+            return document;
         }
     }
 }
---
+++ demo02/Models/News.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo02.Models
+{
+    public class News
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
