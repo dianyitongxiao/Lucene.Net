b1d5fcc86bd6bf2e65da5aa937335447987e723e
shenqiangbin <shenqb@qiezzi.com>
2015-12-28 17:21:24 +0800

    完成基本的搜索功能

--- demo02/Controllers/LuceneNetController.cs
+++ demo02/Controllers/LuceneNetController.cs
@@ -1,7 +1,10 @@
 ﻿using demo02.Models;
+using demo02.Util;
 using Lucene.Net.Analysis.PanGu;
+using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
 using Lucene.Net.Search;
 using Lucene.Net.Store;
 using System;
@@ -14,7 +17,7 @@ using System.Web.Mvc;
 namespace demo02.Controllers
 {
     public class LuceneNetController : Controller
-    {        
+    {
 
         // GET: LuceneNet
         public ActionResult Index()
@@ -25,8 +28,10 @@ namespace demo02.Controllers
         public ActionResult Demo1(string searchKey = "")
         {
             ViewBag.SearchKey = searchKey;
-            Search(searchKey);
-            return View();
+            List<News> news = new List<News>();
+            if (!string.IsNullOrEmpty(searchKey))
+                news = Search(searchKey);
+            return View(news);
         }
 
         public ActionResult CreateIndex()
@@ -35,6 +40,7 @@ namespace demo02.Controllers
             bool isExist = IndexReader.IndexExists(directory);
 
             IndexWriter writer = new IndexWriter(directory, new PanGuAnalyzer(), !isExist, IndexWriter.MaxFieldLength.UNLIMITED);
+            writer.DeleteAll();
 
             GetNews().ForEach(m =>
             {
@@ -48,7 +54,7 @@ namespace demo02.Controllers
         }
 
         private FSDirectory CreateFSDirectory()
-        {            
+        {
             FSDirectory directory = FSDirectory.Open(new DirectoryInfo(GetIndexDataPath()), new NativeFSLockFactory());
 
             bool isExist = IndexReader.IndexExists(directory);
@@ -65,9 +71,9 @@ namespace demo02.Controllers
         {
             List<News> list = new List<News>();
 
-            list.Add(new News { Id = Guid.NewGuid(), Title = "ASP.NET验证问题", Content = "验证什么的是灰常重要的.... ASP.NET" });
-            list.Add(new News { Id = Guid.NewGuid(), Title = "Web API验证问题", Content = "验证什么的是灰常重要的....Web API" });
-            list.Add(new News { Id = Guid.NewGuid(), Title = "ASP.NET登录", Content = "登录问题也是一个大问题" });
+            list.Add(new News { Id = Guid.NewGuid(), Title = "ASP.NET验证问题", Content = "验证什么验证的是灰常重要的验证问题.... " });
+            list.Add(new News { Id = Guid.NewGuid(), Title = "Web API验证问题", Content = "验证什么的是灰常重要的...." });
+            list.Add(new News { Id = Guid.NewGuid(), Title = "ASP.NET登录", Content = "问题也是一个大问题" });
 
             return list;
         }
@@ -83,22 +89,70 @@ namespace demo02.Controllers
             return document;
         }
 
-        private void Search(string searchKey)
+        private News DocumentToNews(string keyword, Document doc)
+        {
+            if (doc == null) return null;
+            News news = new News();
+
+            news.Id = new Guid(doc.Get("id"));
+            news.Title = doc.Get("title");
+            news.Title = SplitContent.HightLight(keyword, news.Title);
+            news.Content = doc.Get("content");
+            news.Content = SplitContent.HightLight(keyword, news.Content);
+
+            return news;
+        }
+
+        private List<News> Search(string searchKey)
         {
             FSDirectory directory = FSDirectory.Open(new DirectoryInfo(GetIndexDataPath()), new NoLockFactory());
             IndexReader reader = IndexReader.Open(directory, true);
             IndexSearcher searcher = new IndexSearcher(reader);
 
-            PhraseQuery query = new PhraseQuery();
-            foreach (var item in Util.SplitContent.SplitWords(searchKey))
-            {
+            //PhraseQuery query = new PhraseQuery();
+            //PhraseQuery query1 = new PhraseQuery();
+            //foreach (var item in Util.SplitContent.SplitWords(searchKey))
+            //{
+            //    query.Add(new Term("content", item));
+            //    query1.Add(new Term("title", item));
+            //}
+            //query.Slop = 100;
 
+            // MultiPhraseQuery queryResult = new MultiPhraseQuery();
+            //// queryResult.Combine(new Query[] { query, query1 });
+
+            // foreach (var item in Util.SplitContent.SplitWords(searchKey))
+            // {
+            //     queryResult.Add(new Term("content", item));
+            //     queryResult.Add(new Term("title", item));
+            // }
+
+            string[] fields = new string[] { "title", "content" };
+            //Occur[] occurs = new Occur[] { Occur.MUST, Occur.MUST };
+            //Query query = MultiFieldQueryParser.Parse(Lucene.Net.Util.Version.LUCENE_30, searchKey, fields, occurs, new PanGuAnalyzer());
+
+            MultiFieldQueryParser parser = new MultiFieldQueryParser(Lucene.Net.Util.Version.LUCENE_30, fields, new PanGuAnalyzer());
+            var query = parser.Parse(searchKey);            
+
+            TopScoreDocCollector collector = TopScoreDocCollector.Create(1000, true);
+            searcher.Search(query, null, collector);
+            ScoreDoc[] docs = collector.TopDocs(0, collector.TotalHits).ScoreDocs; //达到分页效果
+
+            List<News> list = new List<News>();
+
+            foreach (var item in docs)
+            {
+                int docId = item.Doc;
+                Document doc = searcher.Doc(docId);
+                list.Add(DocumentToNews(searchKey, doc));
             }
+
+            return list;
         }
 
         private string GetIndexDataPath()
         {
-            return HttpContext.Server.MapPath("~/IndexData"); 
+            return HttpContext.Server.MapPath("~/IndexData");
         }
     }
 }
--- demo02/Util/SplitContent.cs
+++ demo02/Util/SplitContent.cs
@@ -1,5 +1,7 @@
 ﻿using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.PanGu;
+using Lucene.Net.Analysis.Tokenattributes;
+using PanGu;
 using PanGu.HighLight;
 using System;
 using System.Collections.Generic;
@@ -18,14 +20,27 @@ namespace demo02.Util
             Analyzer analyzer = new PanGuAnalyzer();
             TokenStream tokenStream = analyzer.TokenStream("", new StringReader(content));            
 
-            while (tokenStream.HasAttribute<Token>())
+            while (tokenStream.IncrementToken())
             {
-                list.Add(tokenStream.GetAttribute<Token>().Term);
+                ITermAttribute termAttribute = tokenStream.GetAttribute<ITermAttribute>();
+                list.Add(termAttribute.Term);
             }
 
             return list.ToArray();            
         }
 
-            
+        public static string HightLight(string keyword, string content)
+        {
+            if (!content.ToLower().Contains(keyword.ToLower())) return content;
+
+            SimpleHTMLFormatter simpleHTMLFormatter = 
+                new SimpleHTMLFormatter("<font style=\"font-style:normal;color:#cc0000;\"><b>", "</b></font>");
+
+            Highlighter highlighter = new Highlighter(simpleHTMLFormatter, new Segment());
+            highlighter.FragmentSize = 1000; //设置摘要的字段数
+
+            return highlighter.GetBestFragment(keyword, content);
+        }
+
     }
 }
